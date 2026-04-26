14d2c8dc16b7fb10c0eea9ac5d7c81d846be8316
FATIMa-khan8858 <52031350+FATIMa-khan8858@users.noreply.github.com>
2026-04-26 15:27:27 +0530

    Added build.sh

--- reciepe/reciepe/settings.py
+++ reciepe/reciepe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^f31+c1kvu@&$ctxyungt&th(@i+$xpr^_&*ic20p23n25k^w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "reciepe.urls"
